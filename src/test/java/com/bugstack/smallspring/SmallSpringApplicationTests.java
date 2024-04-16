294e173942b161b12f240a33069a8c284e46dd58
xufan <1575370789@qq.com>
2024-04-16 10:57:16 +0800

    init

---
+++ src/test/java/com/bugstack/smallspring/SmallSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugstack.smallspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
